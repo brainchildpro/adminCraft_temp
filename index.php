8ddda390a6380576cd71b6419545437880b12e63
Your Name <james@brainchildproductions.com>
2012-02-06 23:26:58 -0500

    Added index html basics

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<LINK rel="stylesheet" href="styles/style.css" media="screen"  />
+<LINK rel="shortcut icon" href="" /> <!--- TODO: Add shortcut icon --->
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="description" content="" /> <!---TODO: Add desctiption --->
+<meta name="keywords" content="" /> <!---TODO: Add keywords if needed --->
+<title>iDig Server Manager</title> <!-- TODO: Replace with PHP to grab info from config file/database --->
+
 <?php
-    phpinfo();
+	include 'connection.php';
+    
 ?>
+
+</head>
+<body>
+	<div id="wrapper">
+		<div id="header"></div>
+		<div id="navigation"></div>
+		<div id="content"></div>
+		<div id="footer"></div>
+	</div>
+	
+</body>
+</html>
